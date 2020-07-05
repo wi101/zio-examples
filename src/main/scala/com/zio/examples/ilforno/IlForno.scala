73498dc72070dee41c73606a42f7df9e8672f30e
Wiem Zine El Abidine <wiemzin@gmail.com>
2020-07-05 18:48:23 +0200

    Implement ilforno example (#13)

---
+++ src/main/scala/com/zio/examples/ilforno/IlForno.scala
@@ -0,0 +1,91 @@
+package com.zio.examples.ilforno
+
+import com.zio.examples.ilforno.fridge._
+import zio.clock.Clock
+import zio.duration._
+import zio._
+
+sealed trait Ingredient extends Serializable with Product
+object Ingredient {
+  final case object Tuna extends Ingredient
+  final case object Tomato extends Ingredient
+  final case object Cheese extends Ingredient
+  final case object Onion extends Ingredient
+  final case object Chili extends Ingredient
+  final case object Mushroom extends Ingredient
+}
+
+sealed trait PizzaType {
+  def ingredients: Map[Ingredient, Int]
+}
+
+object PizzaType {
+  import Ingredient._
+
+  case object Tonno extends PizzaType {
+    val ingredients = Map(Tuna -> 1, Tomato -> 3, Cheese -> 1)
+  }
+
+  case object Margherita extends PizzaType {
+    val ingredients = Map(Tomato -> 5, Cheese -> 1)
+  }
+
+  case object QuatreFromage extends PizzaType {
+    val ingredients = Map(Cheese -> 4)
+  }
+
+  case object Vegetarian extends PizzaType {
+    val ingredients = Map(Tomato -> 4, Mushroom -> 8, Chili -> 4, Cheese -> 1)
+  }
+}
+
+case class Request(name: String, pizza: PizzaType)
+
+class System(requests: Queue[Request]) {
+
+  def sendRequest(request: Request): UIO[Unit] = requests.offer(request).unit
+
+  def handleRequests[R](
+    successAction: String => URIO[R, Unit],
+    fallbackAction: (Error, String) => URIO[R, Unit]
+  ): URIO[R with Clock with Fridge, Unit] =
+    (for {
+      request <- requests.take
+      fridge <- ZIO.service[fridge.Service]
+      _ <- fridge.open
+        .flatMap { oldState =>
+          System
+            .preparePizza(request, oldState)
+            .flatMap(newState => fridge.set(newState))
+        }
+        .ensuring(fridge.close)
+        .flatMap(_ => successAction(request.name))
+        .catchAll(e => fallbackAction(e, request.name))
+    } yield ())
+      .repeat(Schedule.spaced(15.seconds) && Schedule.duration(8.hours))
+      .unit
+
+}
+
+object System {
+
+  def start =
+    Queue.bounded[Request](12).map(new System(_))
+
+  def preparePizza(
+    request: Request,
+    ingredients: Map[Ingredient, Int]
+  ): IO[Error, Map[Ingredient, Int]] =
+    request.pizza.ingredients
+      .foldLeft[IO[Error, Map[Ingredient, Int]]](UIO(ingredients)) {
+        case (res, (ingr, count)) =>
+          val availableIngr = ingredients.get(ingr).flatMap { c =>
+            if ((c - count) >= 0) Some(c - count)
+            else None
+          }
+          availableIngr.fold[IO[Error, Map[Ingredient, Int]]](
+            IO.fail(Error.UnavailableIngredient(ingr))
+          )(availableCount => res.map(_.updated(ingr, availableCount)))
+      }
+
+}
---
+++ src/main/scala/com/zio/examples/ilforno/fridge.scala
@@ -0,0 +1,73 @@
+package com.zio.examples.ilforno
+
+import com.zio.examples.ilforno.fridge.Error.WrongState
+import zio._
+import zio.console.Console
+
+object fridge {
+  type Fridge = Has[Service]
+
+  sealed trait Error
+  object Error {
+    case class UnavailableIngredient(ingredient: Ingredient) extends Error
+    case class WrongState(state: State, expected: State) extends Error
+  }
+
+  sealed trait State
+  object State {
+    case object Closed extends State
+    case object Opened extends State
+  }
+
+  case class FridgeWithState(ingredients: Map[Ingredient, Int], state: State)
+  trait Service {
+    protected def currentState: Ref[FridgeWithState]
+    def open: IO[WrongState, Map[Ingredient, Int]]
+    def close: UIO[Unit]
+    def set(newIngredients: Map[Ingredient, Int]): IO[WrongState, Unit]
+    def fridgeState: UIO[FridgeWithState]
+  }
+
+  def checkState(state: State, expected: State): IO[WrongState, State] =
+    if (state == expected) IO.succeed(state)
+    else IO.fail(WrongState(state, expected))
+
+  def live(initialState: FridgeWithState): ZLayer[Console, Nothing, Fridge] =
+    ZLayer.fromServiceM(
+      console =>
+        Ref
+          .make(initialState)
+          .map(
+            state =>
+              new Service {
+                override def currentState: Ref[FridgeWithState] = state
+                override def open: IO[WrongState, Map[Ingredient, Int]] =
+                  for {
+                    fridgeWithState <- currentState.get
+                    _ <- checkState(fridgeWithState.state, State.Closed)
+                    result <- currentState
+                      .updateAndGet(_.copy(state = State.Opened))
+                  } yield result.ingredients
+
+                override def close: UIO[Unit] =
+                  currentState
+                    .updateAndGet(_.copy(state = State.Closed))
+                    .unit *> console.putStrLn(("The fridge is closed."))
+
+                override def set(
+                  newIngredients: Map[Ingredient, Int]
+                ): IO[WrongState, Unit] =
+                  for {
+                    fridgeWithState <- currentState.get
+                    _ <- checkState(fridgeWithState.state, State.Opened)
+                    _ <- currentState
+                      .updateAndGet(_.copy(ingredients = newIngredients))
+                  } yield ()
+
+                override def fridgeState: UIO[FridgeWithState] =
+                  currentState.get
+            }
+        )
+    )
+
+}
